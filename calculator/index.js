7ead3567feb5ca1497dd64caff58a53029ff4143
Sebbys <raynosebastian19@gmail.com>
2022-08-25 23:34:08 +0800

    calc initial commit

---
+++ calculator/index.js
@@ -0,0 +1,27 @@
+let x = 2
+let y = 9
+
+document.getElementById('num1').textContent = x
+document.getElementById('num2').textContent = y
+let cout = document.getElementById('result') 
+
+function add(){
+    res = x + y
+    cout.textContent = "Sum : " + res 
+}
+function sub() {
+    res = x-y
+    cout.textContent = "Sum : " + res
+    console.log(res);
+}
+function div(){
+    res = x/y
+    cout.textContent = "Sum : " + res
+    console.log(res);
+}
+function mult(){
+    res = x*y
+    cout.textContent = "Sum : " + res
+    console.log(res);
+}
+
